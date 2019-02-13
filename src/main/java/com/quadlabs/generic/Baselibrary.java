7d8835c61df947407a5568ec2ec06d3b95dd2bad
Anurag.Upadhyay <Anurag.Upadhyay@Anurag.quadlabs>
2019-02-13 11:43:11 +0530

    base lib changes

--- src/main/java/com/quadlabs/generic/Baselibrary.java
+++ src/main/java/com/quadlabs/generic/Baselibrary.java
@@ -2,11 +2,15 @@ package com.quadlabs.generic;
 
 
 
+import java.util.List;
+
 //Anurag
 
 import java.util.Properties;
+import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -30,7 +34,10 @@ public class Baselibrary {
 	
 	
 	@Parameters("browser")
-	@BeforeClass	
+	@BeforeClass
+	
+	
+	
 	public void precondition(String Browsername)
 	  {
 		
@@ -38,7 +45,7 @@ public class Baselibrary {
 				
 		 {  
 			 
-			 System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"\\exefiles\\chromedriver.exe");
+			 System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"\\exefiles\\chrome\\chromedriver.exe");
 	         driver = new ChromeDriver();
 		     Reporter.log("chrome launched",true);
 		     }
@@ -66,9 +73,9 @@ public class Baselibrary {
 	  
 		 	e_driver = new EventFiringWebDriver(driver);
 		 	
-			// Now create object of EventListerHandler to register it with EventFiringWebDriver
+		//	Now create object of EventListerHandler to register it with EventFiringWebDriver
 			
-		 	eventListener = new WebEventListener();
+		 eventListener = new WebEventListener();
 			
 		 	e_driver.register(eventListener);
 			
@@ -146,10 +153,132 @@ public class Baselibrary {
 	
 
 		
+		public  static List<WebElement> listofautosuggestion(By suggestiontxt, String txt,
+				String city_name, By d) throws InterruptedException {
+			driver.findElement(d).sendKeys(txt);
+			driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+			List<WebElement> autosuggestions = driver.findElements(suggestiontxt);
+			for (WebElement autosuggestion : autosuggestions) {
+				System.out.println(autosuggestion.getText());
+				Thread.sleep(3000);
+				
+				System.out.println("Excel Text"  + " " +city_name);
+				if (autosuggestion.getText().equalsIgnoreCase(city_name)) {
+					System.out.println("equal");
+					autosuggestion.click();
+					break;
+				} else {
+					System.out.println("not equal");
+				}
+			}
+
+			return autosuggestions;
+		}
 		
-		
+		public static  void calender(String date) throws Exception{
 			
+		System.out.println("Trip date"+date);
+
+		String[] tripDateA = date.split("-");
+
+		int tripDay=Integer.parseInt(tripDateA[0]);
 		
+		System.out.println("tripDateA[0]"+tripDateA[0]);
+
+		int tripMon=Integer.parseInt(tripDateA[1]);
+		
+		System.out.println("tripDateA[1]"+tripDateA[1]);
+
+		int tripYear=Integer.parseInt(tripDateA[2]); 
+		
+		System.out.println("tripDateA[2]"+tripDateA[2]);
+		
+		WebElement currentmonth= getWebElement("firstmonth");
+		
+          String value= currentmonth.getText();
+          
+          System.out.println(currentmonth.getText());
+          
+          String[] CalMonYear = value.split(" ");
+          
+          String currentMon = CalMonYear[0];
+          
+          System.out.println(currentMon);
+          int currentMonNum=1;
+         
+          if(currentMon.equalsIgnoreCase("Jan")){
+        	  currentMonNum=1;
+
+        	  }
+        	  else if(currentMon.equalsIgnoreCase("Feb")){
+        	  currentMonNum=2;
+
+        	  }
+        	  else if(currentMon.equalsIgnoreCase("Mar")){
+        	  currentMonNum=3;
+
+        	  }
+        	  else if(currentMon.equalsIgnoreCase("Apr")){
+        	  currentMonNum=4;
+
+        	  }
+        	  else if(currentMon.equalsIgnoreCase("May")){
+        	  currentMonNum=5;
+
+        	  }
+        	  else if(currentMon.equalsIgnoreCase("Jun")){
+        	  currentMonNum=6;
+
+        	  }
+        	  else if(currentMon.equalsIgnoreCase("Jul")){
+        	  currentMonNum=7;
+
+        	  }
+        	  else if(currentMon.equalsIgnoreCase("Aug")){
+        	  currentMonNum=8;
+
+        	  }
+        	  else if(currentMon.equalsIgnoreCase("Sep")){
+        	  currentMonNum=9;
+
+        	  }
+        	  else if(currentMon.equalsIgnoreCase("Oct")){
+        	  currentMonNum=10;
+
+        	  }
+        	  else if(currentMon.equalsIgnoreCase("Nov")){
+        	  currentMonNum=11;
+
+        	  }
+        	  else if(currentMon.equalsIgnoreCase("Dec")){
+        	  currentMonNum=12;
+
+        	  }
+          
+         
+          for (int i = 1; i <= tripMon - currentMonNum; i++) 
+          {
+        	
+        	System.out.println(tripMon-currentMonNum);
+        	
+        	System.out.println(i);
+        	  
+        	WebElement nextmonths = getWebElement("nextmonth");
+        	
+        	JavascriptExecutor js = (JavascriptExecutor) driver;
+        	
+        	js.executeScript("arguments[0].click();",nextmonths);
+        	
+          }
+          
+          Thread.sleep(2000);
+          
+          JavascriptExecutor tripDays = (JavascriptExecutor) driver;
+          
+          WebElement tripdayclick = driver.findElement(By.xpath("(//a[contains(.,'"+tripDay+"')])[1]"));
+          
+          tripDays.executeScript("arguments[0].click();", tripdayclick);
+	}
 	
 }
 
